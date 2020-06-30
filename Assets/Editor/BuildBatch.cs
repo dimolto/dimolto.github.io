1876db35818fd28c1de20e261ee6d48f7f98bfdb
dimolto <aotokage52@gmail.com>
2020-06-30 13:45:58 +0000

    Deploying to master from  @ d2805eae91c89b9257682980c16a71f181aa258c 🚀

---
+++ Assets/Editor/BuildBatch.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class BuildBatch
+{
+    [MenuItem("Build/Build WebGL")]
+    public static void Build()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.locationPathName = "WebGLBuild";
+        buildPlayerOptions.target = BuildTarget.WebGL;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
---
+++ Assets/Editor/Tests/NewTestScript.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+namespace Tests
+{
+    public class NewTestScript
+    {
+        [Test] public void IsTrueTest() => Assert.IsTrue(true);
+        [Test] public void IsFalseTest() => Assert.IsFalse(false);
+        //[Test] public void FailProof() => Assert.IsTrue(false);
+    }
+}
